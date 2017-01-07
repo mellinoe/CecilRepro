f8a35378bf7087a4ecb2884c191b1a2e61220a8c
Eric Mellino <erme@microsoft.com>
2017-01-07 00:37:43 -0800

    Repro code

---
+++ rewriter/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Mono.Cecil;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            AssemblyDefinition.ReadAssembly(args[0]).Write(args[1]);
+        }
+    }
+}
